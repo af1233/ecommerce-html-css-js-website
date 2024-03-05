1913eee45a258860fbaa26dd5c7104bc8ac3771f
Aashiq Farid <146064739+af1233@users.noreply.github.com>
2024-03-05 21:49:08 +0500

    ecommerce website with HTML, CSS, and JavaScript
    "Dynamic ecommerce website crafted with HTML, CSS, and JavaScript for seamless user experience. Utilizes responsive design principles to ensure compatibility across devices. Enhanced with interactive features and animations to engage customers. Optimized for speed and performance to facilitate smooth transactions."

---
+++ E-Commerce-css-js-project/script.js
@@ -0,0 +1,9 @@
+// .........menu bar coding. ............
+function openNav() {
+    document.getElementById("navbar").style.right = "0px";
+    // document.getElementById("main").style.marginLeft = "250px";
+  }
+  function closeNav() {
+    document.getElementById("navbar").style.right = "-300px";
+    // document.getElementById("main").style.marginLeft= "0";
+  }
